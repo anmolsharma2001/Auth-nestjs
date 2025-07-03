d322a4cb777ae9d5518994e80459f1c08039f46e
Anmol Sharma <anmolsharma20012003@gmail.com>
2025-07-03 17:30:24 +0530

    Initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import { RefreshToken } from './schemas/refresh-token.schema';
+import { Body, Controller, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
+import { LoginDto } from './dtos/login.dto';
+import { RefreshTokenDto } from './dtos/refresh-tokens.dto';
+import { ChangePasswordDto } from './dtos/change-password.dtos';
+import { AuthGuard } from 'src/guards/auth.guards';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')      // auth/signup
+  async signUp(@Body() signupData:SignupDto ) {
+     return this.authService.signUp(signupData);
+  }
+
+  @Post('login')
+  async logIn(@Body() credentials: LoginDto){
+    return this.authService.logIn(credentials);
+  }
+
+  @Post('refresh')
+  async refreshTokens(@Body() refreshTokenDto: RefreshTokenDto ){
+    return this.authService.refreshTokens(refreshTokenDto.refreshToken)
+  }
+
+  @UseGuards(AuthGuard)
+  @Put('change-password')
+    async changePassword(
+      @Body() changePasswordDto: ChangePasswordDto,
+      @Req() req) 
+    {
+      return this.authService.changePassword(
+        req.userId,
+        changePasswordDto.oldPassword, 
+        changePasswordDto.newPassword,
+      );
+    }
+  }
+
